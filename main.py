557cbdfb5dc8636033b55fc54653ea4de3e36459
siwoo1123 <61868624+siwoo1123@users.noreply.github.com>
2020-12-25 11:33:15 +0900

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ import time
 pygame.mixer.init()
 pygame.init()
 
-text ="2021년 새해가 밝았습니다."
+allha = time.strftime('%Y', time.localtime(time.time()))
+text = f"{str(int(allha) + 1)}년 새해가 밝았습니다."
 filename = "helloKO.mp3"
 
 tts = gTTS(text=text, lang='ko')
@@ -12,7 +13,7 @@ tts.save(filename)
 while True:
     print(time.strftime('%Z %Y %m %d %p %I %M %S', time.localtime(time.time())))
     time.sleep(1)
-    if time.strftime('%Z %Y %m %d %p %I %M %S', time.localtime(time.time())) == "KST 2020 12 31 PM 11 59 55" or time.strftime("%Z %Y %m %d %p %I %M %S", time.localtime(time.time())) == "대한민국 표준시 2020 12 31 PM 11 59 55":
+    if time.strftime('%Z %Y %m %d %p %I %M %S', time.localtime(time.time())) == f"KST {allha} 12 31 PM 11 59 55" or time.strftime("%Z %Y %m %d %p %I %M %S", time.localtime(time.time())) == f"대한민국 표준시 {allha} 12 31 PM 11 59 55":
         pygame.mixer.music.load("보신각 종소리.mp3")
         pygame.mixer.music.play(loops=0, start=0.0)
         time.sleep(10)
