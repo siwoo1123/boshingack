7984c1a84f838e30117cb5537f76b31fb961497b
siwoo1123 <61868624+siwoo1123@users.noreply.github.com>
2020-12-25 11:55:12 +0900

    Update main.py

--- main.py
+++ main.py
@@ -14,10 +14,11 @@ while True:
     print(time.strftime('%Z %Y %m %d %p %I %M %S', time.localtime(time.time())))
     time.sleep(1)
     if time.strftime('%Z %Y %m %d %p %I %M %S', time.localtime(time.time())) == f"KST {allha} 12 31 PM 11 59 55" or time.strftime("%Z %Y %m %d %p %I %M %S", time.localtime(time.time())) == f"대한민국 표준시 {allha} 12 31 PM 11 59 55":
-        pygame.mixer.music.load("보신각 종소리.mp3")
-        pygame.mixer.music.play(loops=0, start=0.0)
-        time.sleep(10)
-        pygame.mixer.music.set_volume(10)
+        for i in range(1, 34):
+            pygame.mixer.music.load("보신각 종소리.mp3")
+            pygame.mixer.music.play(loops=0, start=0.0)
+            time.sleep(10)
+            pygame.mixer.music.set_volume(10)
         pygame.mixer.music.load(filename)
         pygame.mixer.music.play(loops=0, start=0.0)
         time.sleep(10)
