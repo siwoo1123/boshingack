8e5ed5904f245320ab2882bdcfd0b693227efd43
siwoo1123 <61868624+siwoo1123@users.noreply.github.com>
2020-12-25 10:46:46 +0900

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ tts.save(filename)
 while True:
     print(time.strftime('%Z %Y %m %d %p %I %M %S', time.localtime(time.time())))
     time.sleep(1)
-    if time.strftime('%Z %Y %m %d %p %I %M %S', time.localtime(time.time())) == "KST 2020 12 31 PM 11 59 55":
+    if time.strftime('%Z %Y %m %d %p %I %M %S', time.localtime(time.time())) == "KST 2020 12 31 PM 11 59 55" or time.strftime("%Z %Y %m %d %p %I %M %S", time.localtime(time.time())) == "대한민국 표준시 2020 12 31 PM 11 59 55":
         pygame.mixer.music.load("보신각 종소리.mp3")
         pygame.mixer.music.play(loops=0, start=0.0)
         time.sleep(10)
